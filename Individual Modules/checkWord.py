b654d77520bae9603f9b01819539283982e1b8e6
Sakib Shahriyar Pathan <shaheen840@gmail.com>
2017-10-29 10:47:46 +0600

    Update checkWord.py

--- Individual Modules/checkWord.py
+++ Individual Modules/checkWord.py
@@ -9,16 +9,60 @@ f.close()
 
 scrumbled_word = scrumbled_word.strip()
 list2 = scrumbled_word.split(" ")
-attempt = raw_input("How many times do you want to play? : ")
-attempt = int (attempt)
-for line in range(attempt):
+
+#attempt = raw_input("How many times do you want to play? : ")
+#attempt = int (attempt)
+
+points = 0
+attempt = 0
+lives = 5
+wa = 0
+line = 0
+
+while (lives != 0):
+	attempt = attempt + 1
+	line = line + 1
+
 	ques = random.choice(list2) 
 	ques = ques.strip()
+
 	correct = dic.keys()[dic.values().index(ques)]
-	print ques
+
+	print "Ques no. ", line, ": ", ques
 	ans = raw_input("Answer: ")
 	ans = ans.strip()
+
 	if ans == correct:
-		print "Accepted!"
+		points = points+1
+		print "Accepted! and your point is: ", points, "Lives = ", lives 
+
 	else:
-		print "Sorry! WA!"
+		wa = wa + 1 
+		if(wa % 2 == 0):
+			lives = lives - 1
+		print "Sorry! WA! and your point is: ", points, "Lives = ", lives
+		print "The CA was: ", correct  
+	print " "
+
+print " "
+print "Your final result is: ", points, attempt
+print " "
+
+val = float (points) /attempt
+avg = val*100
+#print avg
+
+if avg == 0:
+	print "Damn, you're so bad, cow!!!!!"
+
+elif avg < 20:
+	print "Nigga have you ever gone to school?"
+
+elif  avg < 50:
+	print "Welcome to school"
+
+elif avg < 75:
+	print "Teach the developer some english, he's so bad"
+
+else: 
+	print "That's some shit right there, you should write your own dictionary"
